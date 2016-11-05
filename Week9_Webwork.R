5c5488f96132304440abc6a4d7e2836ecba7234e
Bean <bean@Bean.local>
2016-11-04 20:21:29 -0400

    finished up webwork problems

--- Week9_Webwork.R
+++ Week9_Webwork.R
@@ -34,10 +34,13 @@ summary(Sex)
 summary(Weight)
 summary(Time)
 typeof(Time)
-
-Time2 = as.numeric(as.character(Time))
+Sex
+Time
+Time2 = as.numeric(as.character(Time)) 
 Time = as.numeric(levels(Time))[Time] 
 Time[1:5]
+Time= Time2
+
 
 summary(Time)
 babyboom[which.max(Time),]
@@ -46,3 +49,59 @@ trunc(Time) == Time
 which(   ( trunc(Time) == Time ) == F   )
 
 ##left off at #10
+Time %% 100 
+
+which(Time%%100 >59|Time>2400) # remainder greater than 59 not possible
+Time[which(Time%%100 >59|Time>2400)] = NA
+
+Time[14]
+Time
+##WEIGHT
+
+POUND = 453.592
+KG = 1000 
+Weight
+Weight/POUND
+Weight/KG
+
+summary(Weight)
+which(Weight<1000)
+
+Weight[which(Weight<1000)] = NA
+
+which(is.na(Time))
+
+Time3 = which(!is.na(Time))
+Time3
+
+sd(Time[Time3])
+
+sd(Time,na.rm=T)
+
+rm(mydata2)
+
+mydata2 = data.frame(Time,Sex,Weight)
+mydata3 = mydata2[complete.cases(mydata2),]#complete.cases adds only records without NA's or NULLS
+mydata3
+dim(mydata3) #37
+
+rm(Time,Sex,Weight)
+
+mydata3$morning = with(
+  mydata3,
+  Time<1200
+) #add column to tell whether baby was born in morning
+
+length(which(mydata3$morning==T))
+
+sort(mydata3$Time) 
+library(sqldf)
+
+query = "select Weight from mydata3 order by Time"
+
+sqldf(query)
+library(reshape2) #used to transpose, look on p.200
+
+
+
+
